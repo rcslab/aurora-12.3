f3fb0007e6bfe8ed846822b1ffca2aa23cecf1f2
Emil Tsalapatis <emil.tsalapatis@uwaterloo.ca>
2022-05-01 21:50:53 -0400

    made global SYSV shmsegs public

--- sys/kern/sysv_shm.c
+++ sys/kern/sysv_shm.c
@@ -115,9 +115,9 @@ static MALLOC_DEFINE(M_SHM, "shm", "SVID compatible shared memory segments");
 #define	SHMSEG_REMOVED  	0x0400
 #define	SHMSEG_ALLOCATED	0x0800
 
-static int shm_last_free, shm_nused, shmalloced;
+int shm_last_free, shm_nused, shmalloced;
 vm_size_t shm_committed;
-static struct shmid_kernel *shmsegs;
+struct shmid_kernel *shmsegs;
 static unsigned shm_prison_slot;
 
 struct shmmap_state {
